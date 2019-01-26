8978975e8320c0c3606150ca04c27afac64b25a1
Omar Hydrah <omarhydrah@tutanota.de>
2019-01-26 22:18:59 +0200

    Initial Commit

---
+++ web/src/main/java/com/etca/EtcaApplication.java
@@ -0,0 +1,14 @@
+package com.etca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtcaApplication.class, args);
+	}
+
+}
+
