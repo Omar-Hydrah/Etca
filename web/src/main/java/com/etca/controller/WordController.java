cd5349b1df66219227e17162327da3c9ae086856
Omar Hydrah <omarhydrah@tutanota.de>
2019-02-07 19:55:41 +0200

    Finish create-word form

--- web/src/main/java/com/etca/controller/WordController.java
+++ web/src/main/java/com/etca/controller/WordController.java
@@ -1,6 +1,7 @@
 package com.etca.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,33 +11,67 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Collection;
 
 import com.etca.model.Word;
 import com.etca.model.User;
 import com.etca.model.RoleEnum;
+import com.etca.model.Language;
+import com.etca.model.Level;
+import com.etca.form.WordForm;
+import com.etca.service.LanguageService;
+import com.etca.service.LevelService;
+import com.etca.service.WordService;
 
 @RequestMapping("/word")
 @Controller
 public class WordController{
 
+	private static final Logger logger =
+		LoggerFactory.getLogger(WordController.class);
+
+	@Autowired
+	private LevelService levelService;
+
+	@Autowired
+	private LanguageService languageService;
+
+	@Autowired
+	private WordService wordService;
+
+
 	@GetMapping({"/", "/index"})
 	public String all(){
+		List<Word> words = wordService.findAll();
 		return "word/words";
 	}
 
 	@GetMapping("/create-word")
 	public String createWord(Model model){
-		model.addAttribute(new Word());
+
+		String[] languages = languageService.findAllArray();
+		String[] levels    = levelService.findAllArray();
+
 		
+		model.addAttribute("wordForm", new WordForm());
+		model.addAttribute("languages", languages);
+		model.addAttribute("levels", levels);
+
 		return "word/create-word";
 	}
 
 	@PostMapping("/create-word")
-	public String createWord(@Valid @ModelAttribute Word word, 
+	public String createWord(@Valid @ModelAttribute WordForm wordForm, 
 		RedirectAttributes redirect, BindingResult result, Model model,
 		@AuthenticationPrincipal User principal)
 	{	
+		// logger.info("\n\n" + wordForm.toString() + "\n\n");
+
 		if(!principal.canCreateContent()){
 
 			redirect.addFlashAttribute("flash", "Must be an author to create word");
