90c161bb7b25f1a2301f526a9ed638100b8b09f3
Omar Hydrah <omarhydrah@tutanota.de>
2019-02-09 13:39:29 +0200

    Using an insert statement with multiple selects in WordRepository

--- web/src/main/java/com/etca/repo/WordRepository.java
+++ web/src/main/java/com/etca/repo/WordRepository.java
@@ -2,6 +2,8 @@ package com.etca.repo;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.etca.model.Word;
 
@@ -12,4 +14,10 @@ public interface WordRepository extends JpaRepository<Word, Long>{
 
 	List<Word> findAll();
 	Word save(Word word);
+
+	@Query(value=
+		"insert into word(word, language, level_id) values(:word, (select id from language where language = :language),(select id from level where title= :level))", nativeQuery=true)
+	Word save(
+		@Param("word") String word, 
+		@Param("language") String language, @Param("level") String level);
 }
