918fa48b566ef9c0951f4047e0fa8a44829b811c
Omar Hydrah <omarhydrah@tutanota.de>
2019-03-01 16:46:57 +0200

    Create API end points for lessons

--- web/src/main/java/com/etca/controller/rest/LessonRestController.java
+++ web/src/main/java/com/etca/controller/rest/LessonRestController.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.etca.model.Lesson;
 import com.etca.service.LessonService;
@@ -17,24 +23,39 @@ import com.etca.service.LessonService;
 @RequestMapping("/api/lesson")
 public class LessonRestController{
 
+	private static final Logger log = 
+		LoggerFactory.getLogger(LessonRestController.class);
+
 	@Autowired
 	private LessonService lessonService;
 
+	@GetMapping("/")
+	public Map<String, String> index(){
+		Map<String, String> links = new HashMap<>();
+
+		links.put("all lessons", "/api/lesson/all");
+		links.put("single lesson", "/api/lesson/{id}");
+		return links;
+	}
+
 	@GetMapping("/{id}")
-	@ResponseBody
-	public Lesson lesson(){
-		Optional<Lesson> lessonOptional = lessonService.findById();
+	public Lesson lesson(@PathVariable("id") Long id){
+		Optional<Lesson> lessonOptional = lessonService.findById(id);
 
 		if(lessonOptional.isPresent()){
-			return lessonOptional.get();
+			Lesson lesson = lessonOptional.get();
+
+			return lesson;
 		}else{
 			return null;
 		}
 	}
 
 	@GetMapping("/all")
-	@ResponseBody
 	public List<Lesson> all(){
-		return lessonService.findAll();
+
+		List<Lesson> lessons = lessonService.findAll();
+
+		return lessons;
 	}
 }
