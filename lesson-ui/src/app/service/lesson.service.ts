1f23c3f44429dafe2ed0b6383a962952d949b827
Omar Hydrah <omarhydrah@tutanota.de>
2019-03-02 12:23:10 +0200

    Fix some errors in lesson service

--- lesson-ui/src/app/service/lesson.service.ts
+++ lesson-ui/src/app/service/lesson.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
+import { catchError } from "rxjs/operators";
 import { Lesson } from "../lesson.model";
 import { Sentence } from "../sentence.model";
 
@@ -10,13 +11,22 @@ import { Sentence } from "../sentence.model";
 })
 export class LessonService {
 
-    constructor(private httpClient : HttpClient) {}
+    constructor(private http : HttpClient) {}
 
-    getLesson(id : number) : any{
-
+    getLesson(id : number) : Observable<Lesson>{
+    	return this.http.get<Lesson>("/api/lesson/" + id)
+    		.pipe(catchError(this.handleError<Lesson>("getLesson")));
     }
 
-    getLessons(): any{
+    getLessons(): Observable<Lesson[]>{
+    	return this.http.get<Lesson[]>("/api/lesson/all")
+    		.pipe(catchError(this.handleError<Lesson[]>("getLesssons", [])));
+    }
 
+    private handleError<T>(operation="operation", result ? : T){
+    	return (error : any) : Observable<T> =>{
+    		console.error(error);
+    		return of(result as T);
+    	}
     }
 }
