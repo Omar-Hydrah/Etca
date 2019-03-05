a74c3c99b80f4b577ab25507de6d576181b21a39
Omar Hydrah <omarhydrah@tutanota.de>
2019-03-05 22:29:54 +0200

    Reorganize routes

--- lesson-ui/src/app/app-routing.module.ts
+++ lesson-ui/src/app/app-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LessonComponent } from "./lesson/lesson.component";
-import { MainComponent } from "./main/main.component";
-import { AppComponent } from "./app.component";
+import { LessonComponent }   from "./lesson/lesson.component";
+import { MainComponent }     from "./main/main.component";
+import { ViewComponent }     from "./view/view.component";
+import { AppComponent }      from "./app.component"; 
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { BlankComponent }    from "./blank/blank.component";
 
 const routes: Routes = [
-	{path: ":id", component: MainComponent},
-	{path: ":id/learn", component: LessonComponent},
-	{path: "", component: AppComponent},
+	{path: "not-found", component: NotFoundComponent},
+	{path: ":id", component: ViewComponent},
+	{path: "", component: BlankComponent, pathMatch: "full"},
+	{path: "**", component: NotFoundComponent},
 ];
 
 @NgModule({
