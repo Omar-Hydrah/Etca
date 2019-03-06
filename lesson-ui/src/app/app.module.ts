2ef180039c8ead5a2a1eff9678cc13d62ca17a9f
Omar Hydrah <omarhydrah@tutanota.de>
2019-03-06 09:33:00 +0200

    Import FormsModule

--- lesson-ui/src/app/app.module.ts
+++ lesson-ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
+import { FormsModule }      from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +27,7 @@ import { SentenceComponent } from './sentence/sentence.component';
         BrowserModule,
         AppRoutingModule,
         HttpClientModule,
+        FormsModule,
     ],
     providers: [
         LessonService,
